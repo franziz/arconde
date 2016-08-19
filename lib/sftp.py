84d6989b1723689621661056fbc75586ec6d5808
Frans <franssiswanto@gmail.com>
2016-08-19 23:00:06 +0800

    Solve indent problems

--- lib/sftp.py
+++ lib/sftp.py
@@ -10,16 +10,16 @@ class SFTP:
 		self.sftp.mkdir(path)
 
 	def put_dir(self, source=None, target=None):
+	""" Copy a whole directory in a recursive way. 
+		This function assume that all the source and target are in linux format.
+		Therefore, os.path.join() is not implemented.
+	"""
 		assert source is not None, "source is not defined."
 		assert target is not None, "target is not defined."
 
-		""" Copy a whole directory in a recursive way. 
-	        This function assume that all the source and target are in linux format.
-	        Therefore, os.path.join() is not implemented.
-	    """
-	    for item in os.listdir(source):
-	        if os.path.isfile(os.path.join(source,item)):
-	            sftp.put("{}/{}".format(source,item), "{}/{}".format(target,item))
-	        else:
-	            sftp.mkdir("{}/{}".format(target, item), 511)
-	            put_dir(sftp, "{}/{}".format(source,item), "{}/{}".format(target,item))
+		for item in os.listdir(source):
+			if os.path.isfile(os.path.join(source,item)):
+				sftp.put("{}/{}".format(source,item), "{}/{}".format(target,item))
+			else:
+				sftp.mkdir("{}/{}".format(target, item), 511)
+				put_dir(sftp, "{}/{}".format(source,item), "{}/{}".format(target,item))
