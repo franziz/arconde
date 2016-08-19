a1def6323c18d49a1624a202599e1850e9bea9c9
Frans <franssiswanto@gmail.com>
2016-08-19 14:24:24 +0800

    Initial commit

--- listeners/engine_source.py
+++ listeners/engine_source.py
@@ -1,7 +1,8 @@
 from . import Listener
 import falcon
+import json
 
 class EngineSourceListener(Listener):
 	def on_post(self, req, res):
 		res.status = falcon.HTTP_200
-		res.body   = req.context
+		res.body   = json.dumps(req.context)
