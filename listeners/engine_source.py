e38af9323572085c760e9f5417ca2a7bb733470a
Frans <franssiswanto@gmail.com>
2016-08-19 14:30:58 +0800

    Initial commit

--- listeners/engine_source.py
+++ listeners/engine_source.py
@@ -4,5 +4,9 @@ import json
 
 class EngineSourceListener(Listener):
 	def on_post(self, req, res):
+		payload = req.stream.read()
+		if not payload:
+			raise falcon.HTTPBadRequest("Empty request body","A valid JSON document is required.")
+		payload = payload.decode("utf-8")
 		res.status = falcon.HTTP_200
-		res.body   = json.dumps(req.context)
+		res.body   = payload
